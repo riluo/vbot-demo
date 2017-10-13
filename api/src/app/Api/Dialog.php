c930c779dc6e6f3db4ec3dbcf88b7d9312b4eb0d
赵亮 <zhaoliang@zhaoliangdeMacBook-Pro-2.local>
2017-10-13 15:19:17 +0800

    insert dialog

--- api/src/app/Api/Dialog.php
+++ api/src/app/Api/Dialog.php
@@ -43,15 +43,72 @@ class Dialog extends Api {
         $config_model = new model_config();
         $config_config = $config_model->lists()->order('id DESC')->fetch();
 
-        $selfNickName = $config_config['nickname'];
-        $selfUserName = $config_config['username'];
-        
 
-        $model = new model_dialog();
+        //send to weixin
+        $Uin = $config_config['Uin'];
+        $Sid = $config_config['Sid'];
+        $Skey = $config_config['Skey'];
+        $DeviceID = $config_config['DeviceID'];
+        $pass_ticket = $config_config['pass_ticket'];
+        $username = $config_config['username'];
+        $nickname = $config_config['nickname'];
 
-        $data = array('Type' => '1', 'FromUserName' => $selfUserName, 'FromNickName' => $selfNickName, 'ToUserName' => $this->username, 'ToNickName' => $this->nickname, 'Content' => $this->content, 'CreateTime' => date("Y-m-d H:i:s",time()));
-        $model->lists()->insert($data);
+        $ToUserName = $this->username;
+        $ToNickName = $this->nickname;
 
-        return $model->lists()->insert_id();;
+        $url = "https://wx.qq.com/cgi-bin/mmwebwx-bin/webwxsendmsg?pass_ticket=".$pass_ticket;
+        $arr = array(
+            "BaseRequest" => array(
+                "Uin" => $Uin,
+                "Sid" => $Sid,
+                "Skey"=> $Skey,
+                "DeviceID"=> $DeviceID
+            ),
+            "Msg" => array(
+                "Type"=>1,
+                "Content"=>$this->content,
+                "FromUserName"=>$username,
+                "ToUserName"=> $ToUserName,//NickName:撸货买买买
+                "LocalID"=>time() * 1e4,
+                "ClientMsgId"=>time() * 1e4
+            ));
+
+
+        $jsonStr = json_encode($arr,JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+        list($returnCode, $returnContent) = http_post_json($url, $jsonStr);
+
+        if($returnCode == 200){
+            //save to db
+            $selfNickName = $config_config['nickname'];
+            $selfUserName = $config_config['username'];
+            $model = new model_dialog();
+
+            $data = array('Type' => '1', 'FromUserName' => $selfUserName, 'FromNickName' => $selfNickName, 'ToUserName' => $this->username, 'ToNickName' => $this->nickname, 'Content' => $this->content, 'CreateTime' => date("Y-m-d H:i:s",time()));
+            $model->lists()->insert($data);
+
+            return $model->lists()->insert_id();
+        }
+        return null;
+
     }
+
+    private function http_post_json($url, $jsonStr)
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonStr);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+                'Content-Type: application/json; charset=utf-8',
+                'Content-Length: ' . strlen($jsonStr)
+            )
+        );
+        $response = curl_exec($ch);
+        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
+
+        return array($httpCode, $response);
+    }
+
 }
