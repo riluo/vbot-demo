cb6d42f297aedb5fa6bd70e8a92dfd5f3ad5f1b1
赵亮 <zhaoliang@zhaoliangdeMacBook-Pro-2.local>
2017-10-14 14:35:27 +0800

    adjust config

--- vendor/hanson/vbot/src/Core/Server.php
+++ vendor/hanson/vbot/src/Core/Server.php
@@ -255,10 +255,12 @@ class Server
         $this->vbot->console->log('End');
 
         $pdo = new \PDO("mysql:host=localhost;dbname=sd_chat","root","Sunland16");
+        echo "SELECT count(*) as count from config where Uin = ".$data['wxuin'];
         $q = $pdo->query("SELECT count(*) as count from config where Uin = ".$data['wxuin']);
         $q->setFetchMode(\PDO::FETCH_ASSOC);
 
         $rows = $q->fetch();
+        echo $rows["count"];
         if($rows["count"]>0) {
             $pdo->exec("UPDATE config set Sid='".$data['wxsid']."',Skey='".$data['skey']."',DeviceID='".$this->vbot->config['server.deviceId']."',pass_ticket='".$data['pass_ticket']."',UpdateTime='".date("Y-m-d H:i:s",time())."' where Uin = '".$data['wxuin']."'");
         } else {
