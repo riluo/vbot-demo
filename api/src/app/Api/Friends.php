ef43b9b19cb3dbe410497fd78243701596155f92
赵亮 <zhaoliang@zhaoliangdeMacBook-Pro-2.local>
2017-10-16 16:39:04 +0800

    add small icon to friends

--- api/src/app/Api/Friends.php
+++ api/src/app/Api/Friends.php
@@ -45,27 +45,31 @@ class Friends extends Api {
         $recentChatFromFriends = $model_dialog->getObject()->select('DISTINCT FromNickName as NickName,max(CreateTime) as lastTime')->where("ToNickName",$selfNickName)->group('FromNickName')->order('CreateTime DESC')->fetchAll();
         $recentChatToFriends = $model_dialog->getObject()->select('DISTINCT ToNickName as NickName,max(CreateTime) as lastTime')->where("FromNickName",$selfNickName)->group('ToNickName')->order('CreateTime DESC')->fetchAll();
         $recentFriends = array_merge($recentChatToFriends,$recentChatFromFriends);
+        $lastTime = [];
         foreach ($recentFriends as $key => $row) {
             $lastTime[$key]  = $row['lastTime'];
         }
+        if(count($lastTime)>0){
+            // 将数据根据 lastTime 降序排列，可能有重复的人名，不用管
+            array_multisort($lastTime, SORT_DESC,$recentFriends);
 
-        // 将数据根据 lastTime 降序排列，可能有重复的人名，不用管
-        array_multisort($lastTime, SORT_DESC,$recentFriends);
-
-        //按照聊天顺序排序
-        $sortedFriends = [];
-        foreach($recentFriends as $v){
-            foreach($friends as $k =>$f){
-                if($f['NickName'] == $v['NickName']){
-                    array_push($sortedFriends, $f);
-                    unset($friends[$k]);
+            //按照聊天顺序排序
+            $sortedFriends = [];
+            foreach($recentFriends as $v){
+                foreach($friends as $k =>$f){
+                    if($f['NickName'] == $v['NickName']){
+                        array_push($sortedFriends, $f);
+                        unset($friends[$k]);
+                    }
                 }
             }
-        }
-        //将未在最近聊天的好友插入列表
-        array_push($sortedFriends, $friends);
+            //将未在最近聊天的好友插入列表
+            array_push($sortedFriends, $friends);
 
-        return $sortedFriends;
+            return $sortedFriends;
+        }
+        return $friends;
+
     }
 
 
